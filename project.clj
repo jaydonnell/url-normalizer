1d2176e35dd0e9241e0f6b07278456f57b166499
Nate Murray <nate@natemurray.com>
2010-11-30 08:17:58 +0800

    bump 0.0.3

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject url-normalizer "0.0.2"
+(defproject url-normalizer "0.0.3"
   :description "url-normalizer"
   :dependencies [[org.clojure/clojure "1.2.0"]
                  [org.clojure/clojure-contrib "1.2.0"]]
